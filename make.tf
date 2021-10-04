b5e6699ecf7220f33955313c4335db7b163a59a7
Konrad Semsch <konrad.semsch@innogy.com>
2021-10-04 09:17:39 +0200

    completed first code version; testing first deployment

--- make.tf
+++ make.tf
@@ -6,13 +6,15 @@ provider "azurerm" {
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = var.location
+  tags     = var.tags
 }
 
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.main.name
+  tags                = var.tags
 }
 
 resource "azurerm_subnet" "main" {
@@ -24,7 +26,7 @@ resource "azurerm_subnet" "main" {
 
 resource "azurerm_network_security_group" "main" {
   name                = "${var.prefix}-security-group"
-  location            = azurerm_resource_group.main.location
+  location            = var.location
   resource_group_name = azurerm_resource_group.main.name
 
   security_rule {
@@ -50,23 +52,40 @@ resource "azurerm_network_security_group" "main" {
     source_address_prefix      = "VirtualNetwork"
     destination_address_prefix = "VirtualNetwork"
   }
-
-  # tags = {
-  #   project = "udacity-ws"
-  # }
+  tags = var.tags
 }
 
 resource "azurerm_public_ip" "main" {
   name                = "${var.prefix}-public-ip"
   resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  allocation_method   = "Dynamic"
+  location            = var.location
+  allocation_method   = "Static"
+  tags                = var.tags
+}
+
+resource "azurerm_lb" "main" {
+  name                = "${var.prefix}-load-balancer"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    subnet_id            = azurerm_subnet.main.id
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_lb_backend_address_pool" "main" {
+  loadbalancer_id = azurerm_lb.main.id
+  name            = "BackEndAddressPool"
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-network-interface"
   resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  location            = var.location
 
   ip_configuration {
     name                          = "internal"
@@ -74,62 +93,49 @@ resource "azurerm_network_interface" "main" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.main.id
   }
-}
 
-resource "azurerm_lb" "main" {
-  name                = "${var.prefix}-load-balancer"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress" ### MODIFY
-    subnet_id            = azurerm_subnet.main.id
-    public_ip_address_id = azurerm_public_ip.main.id
-  }
-}
-
-resource "azurerm_lb_backend_address_pool" "main" {
-  loadbalancer_id = azurerm_lb.main.id
-  name            = "BackEndAddressPool" ### MODIFY
+  tags = var.tags
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "main" {
   network_interface_id    = azurerm_network_interface.main.id
-  ip_configuration_name   = "testconfiguration1" ### MODIFY
+  ip_configuration_name   = "${var.prefix}-ni-backend-address-pool-association"
   backend_address_pool_id = azurerm_lb_backend_address_pool.main.id
 }
 
-resource "azurerm_availability_set" "main" {
-  name                = "${var.prefix}-availability-set"
-  location            = azurerm_resource_group.main.location
+data "azurerm_image" "main" {
+  name                = var.packer_image_name
   resource_group_name = azurerm_resource_group.main.name
+}
 
-  # tags = {
-  #   environment = "Production"
-  # }
+resource "azurerm_availability_set" "main" {
+  name                         = "${var.prefix}-availability-set"
+  location                     = var.location
+  resource_group_name          = azurerm_resource_group.main.name
+  platform_fault_domain_count  = 3
+  platform_update_domain_count = var.n_update_domins
+  tags                         = var.tags
 }
 
 resource "azurerm_linux_virtual_machine" "main" {
   name                            = "${var.prefix}-vm"
   resource_group_name             = azurerm_resource_group.main.name
-  location                        = azurerm_resource_group.main.location
-  size                            = "Standard_D2s_v3"
-  admin_username                  = "konradino" # "${var.username}"
-  admin_password                  = "Udacity202!" # "${var.password}"
+  availability_set_id             = azurerm_availability_set.main.id
+  location                        = var.location
+  size                            = "Standard_DS1_v2"
+  source_image_id                 = data.azurerm_image.main.id
+  admin_username                  = "${var.admin_username}"
+  admin_password                  = "${var.admin_password}"
   disable_password_authentication = false
-  network_interface_ids = [
-    azurerm_network_interface.main.id,
-  ]
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
 
   os_disk {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
   }
+  
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  tags = var.tags
 }
--- vars.tf
+++ vars.tf
@@ -1,11 +1,39 @@
 variable "prefix" {
+  description = "The prefix which should be used for all resources in this project"
   default = "project-web-server"
   type = string
-  description = "The prefix which should be used for all resources in this project."
 }
 
 variable "location" {
+  description = "The Azure Region in which all resources in this project should be created"
   default = "West Europe"
   type = string
-  description = "The Azure Region in which all resources in this project should be created."
+}
+
+variable "packer_image_name" {
+  description = "Name of the Packer image"
+  default = "project-web-server-packer-image"
+}
+
+variable "n_update_domins" {
+  description = "The number of update domains of the VM in the availability set"
+  default     = 5
+  type        = number
+}
+
+variable "admin_user" {
+   description = "User name to use as the admin account on the VMs that will be part of the VM scale set"
+   default     = "konradino"
+}
+
+variable "admin_password" {
+   description = "Default password for admin account"
+}
+
+variable "tags" {
+   description = "Map of the tags to use for the resources that are deployed"
+   type        = map(string)
+   default = {
+      project = "udacity-ws"
+   }
 }
